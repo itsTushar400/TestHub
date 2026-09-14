e635d0b6c36ec719c290d4917bc48c0ee79d1199
itsTushar400 <tusharchauhan400@gmail.com>
2026-09-14 14:32:44 +0530

    Fix Aiven MySQL connection

--- backend/db.js
+++ backend/db.js
@@ -4,9 +4,15 @@ const mysql = require("mysql2");
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -14,13 +20,11 @@ const db = mysql.createPool({
 
 db.getConnection((err, connection) => {
   if (err) {
-    console.log("❌ MySQL Connection Failed");
-    console.log(err.message);
+    console.error("❌ MySQL Connection Failed:", err.message);
     return;
   }
 
-  console.log("✅ MySQL Connected Successfully");
-  console.log("📦 Database:", process.env.DB_NAME);
+  console.log("✅ MySQL Connected Successfully!");
 
   connection.release();
 });
